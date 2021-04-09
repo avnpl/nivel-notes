cf4b9cb8388f71af7eea8368d75f4522216fd022
Alvin Pauly <alvinpaul1311@gmail.com>
2021-04-09 23:13:32 +0530

    Initiated database implementation

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'qwertyuiopasdfgh'
-    app.secret_key = 'qwertyuiopasdfgh'
-    
+    app.config['SECRET_KEY'] = 'qwertyuiopasdfgh'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
 
