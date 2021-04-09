a26ded517cc50ecdee35c669030db325659bd7c5
Alvin Pauly <alvinpaul1311@gmail.com>
2021-04-09 23:40:05 +0530

    Made the database models

--- website/auth.py
+++ website/auth.py
@@ -3,17 +3,20 @@ from flask.helpers import flash
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
+
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
+
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    if request.method=='POST':
+    if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
@@ -24,7 +27,7 @@ def sign_up():
             flash('Email should be more than 3 characters long', category='error')
         elif len(firstName) < 2:
             flash('First name should be more than 1 characters long', category='error')
-        elif password1!=password2:
+        elif password1 != password2:
             flash('Passwords don\'t match', category='error')
         elif len(password1) < 7:
             flash('Password should be more than 6 characters long', category='error')
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from typing import KeysView
 from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
