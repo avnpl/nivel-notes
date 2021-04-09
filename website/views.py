a6b6227cd216886539e163b534a68397b7e70b9b
Alvin Pauly <alvinpaul1311@gmail.com>
2021-04-09 20:30:57 +0530

    Basic implementation of Login and Signup page done

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from typing import KeysView
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return '<h1>Test</h1>'
+    return render_template("home.html")
