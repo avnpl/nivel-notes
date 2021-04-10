57cd82ea5300e267c8023f67aea4f3c8b1c90423
Alvin Pauly <alvinpaul1311@gmail.com>
2021-04-10 14:02:31 +0530

    Users are now added to database while signing up

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask.helpers import flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -18,20 +21,24 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        print(email, firstName, password1, password2)
+        print(email, first_name, password1, password2)
 
         if len(email) < 4:
             flash('Email should be more than 3 characters long', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name should be more than 1 characters long', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error')
         elif len(password1) < 7:
             flash('Password should be more than 6 characters long', category='error')
         else:
+            new_user = User(email = email, first_name = first_name, password = generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
